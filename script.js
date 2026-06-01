bb842eb98b37fc10092a5c28e353e32381f016c0
jashandeep369 <jdeep.singh2008@gmail.com>
2026-06-01 13:14:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+              "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+              0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+              "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",
+              ",","|",":",";","<",">",".","?"]
+
+let BTN = document.getElementById("btn")
+
+function generate() {
+    let password1 = ""
+    for (let i=0;i<15;i++) {
+        let random = Math.floor(Math.random() * characters.length)
+        password1 += characters[random]
+    }
+    document.getElementsByClassName("pass1")[0].textContent = "> " + password1
+
+    let password2 = ""
+    for (let i=0;i<15;i++) {
+        let random2 = Math.floor(Math.random() * characters.length)
+        password2 += characters[random2]
+    }
+    document.getElementsByClassName("pass2")[0].textContent = "> " + password2
+}
